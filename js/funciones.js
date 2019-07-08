13048cdd6b905bcc77d20cea5fdebbd11daaa977
Daniel Paz <ludapazs@gmail.com>
2019-07-08 08:59:28 -0500

    jh

--- js/funciones.js
+++ js/funciones.js
@@ -2,16 +2,17 @@ function recibir()
 {
     var valor = document.getElementById("input_texto").value;
     var newDiv = document.createElement("div");
-    newDiv.appendChild(valor);
+    var newContent = document.createTextNode(valor); 
+    newDiv.appendChild(newContent);
     var currentDiv = document.getElementById("antesDe");
-    document.body.insertBefore(newDiv, currentDiv); 
+    document.body.insertBefore(newDiv, currentDiv);
 } 
 
 function addElement () { 
     // crea un nuevo div 
     // y añade contenido 
      
-    var newContent = document.createTextNode("Hola!¿Qué tal?"); 
+    
     newDiv.appendChild(newContent); //añade texto al div creado. 
   
     // añade el elemento creado y su contenido al DOM 
